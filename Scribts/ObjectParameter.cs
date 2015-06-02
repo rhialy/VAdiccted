3e87556c8913cbd750d662d97a1d1f52c9802216
Daniel Stehlik <marcostehlik@web.de>
2015-06-02 21:36:24 +0200

    Okay, hier ist das System wie wir konkret die ganzen GameObject "Veränderungen" behandeln. Ausführliche Ausführung schreibe ich in Slack und es steht auch natürlich als Comment in der Klasse selbst.

---
+++ Scribts/ObjectParameter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectParameter : Main {
+
+	/*********************************************************************|
+	| Dieses Script dient als "Handler" für alle kleineren Scripts die    |
+	| mit der Änderung von Objekten zu tun haben. Dies dient nur der      |
+	| Übersicht, da wenn alle Funktionen für allle sich verändernden      |
+	| Objekte mit den jeweiligen public variables in diesem Script wären  |
+	| würde die Übersichtlichkeit stark darunter leiden.				  |
+	| Wichtig ist, dass alle diese Scripts Child Klassen dieser Klasse	  |
+	| sein müssen, um die notwendigen Variablen zu erben.				  |
+	|*********************************************************************/
+
+	// Hier werden die verschiedenen Scripts gelistet, die initialisiert werden sollen:
+	public TestScript testScript;
+
+	// Die folgenden GOs sind notwendig, damit wir festlegen können bei welchem
+	// Trigger welches Script für welche Objekte ausgelöst wird.
+	public GameObject testActivatedGameObject;
+
+	// ArrayList for deactivating all trigger GOs at start
+	private GameObject [] iniDeactivationGO;
+
+	// Use this for initialization
+	void Start () {
+		// ini all GOs as not active
+		iniDeactivationGO = GameObject.FindGameObjectsWithTag ("ObjectActivationTrigger");
+		foreach (GameObject deactivation in iniDeactivationGO) {
+			deactivation.SetActive (false);
+		}
+		//testScript = GetComponent<TestScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Wenn testTrigger is active, dann tue dies und das
+		if (testActivatedGameObject.activeSelf) {
+			testScript.enabled = true;
+		}
+	}
+
+
+}
