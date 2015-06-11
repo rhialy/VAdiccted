54aa796463758b640eadff5a1aea3a5b56132fab
Daniel Stehlik <marcostehlik@web.de>
2015-06-11 11:09:47 +0200

    Update to the object parameter scripts and added the first test script for objects. Next: Object Parameter uses parameter from main.

--- Scribts/ObjectScripts/TestScript.cs
+++ Scribts/ObjectScripts/TestScript.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class TestScript : ObjectParameter {
+
+	public GameObject movingObject;
+
+	private int counter;
 
 	// Use this for initialization
 	void Start () {
-	
+		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print ("isActive");
+		if(counter < 500) {
+			movingObject.transform.position += new Vector3(0.01f, 0, 0);
+			counter++;
+		}
 	}
 }
