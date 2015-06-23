790ac9bdf304124de0d472fa8be4e0be6594336d
Thamar <Thamar-vainstain@web.de>
2015-06-23 14:51:18 +0200

    player mit herz und schritt

--- Scribts/PlayerController.cs
+++ Scribts/PlayerController.cs
@@ -15,25 +15,45 @@ public class PlayerController : MonoBehaviour
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
 
-	public AudioClip Footsteps;
+	private bool LSD;
+	private bool Ecstasy;
+	//Soundelemente
+	public AudioSource HSource;
+	public AudioSource FSource;
 	public Soundbehavior Sound;
+	private int c = 0;
 	
 	// Use this for initialization
 	void Start()
 	{
+		LSD = Main.Parameters[0];
+		Ecstasy = Main.Parameters[2];
 		controller = GetComponent<CharacterController>();
+
 
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
+
 		if (controller.isGrounded && controllable)
 		{	
 			moveDirection = new Vector3(Input.GetAxis("Vertical")*-1, 0, Input.GetAxis("Horizontal"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
-			Sound.PlaySound(Footsteps);
+
+
+			HSource=Sound.PlaySound (HSource);
+
+
+			if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
+
+				FSource= Sound.PlaySound(FSource);
+
+			} else {
+				Sound.StopSound (FSource);
+			}
 
 			while (Input.GetKeyDown (KeyCode.LeftShift)) {
 				moveDirection *= runningSpeed;
@@ -46,6 +66,11 @@ public class PlayerController : MonoBehaviour
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 
+	}
+	public void Heart(){
+		if (LSD == true || Ecstasy == true) {
+			Sound.speed (HSource, 1.8F, true);
+		}
 	}
 
 }
